a9391aa113434f84895321bf89d751c0129fdf17
Seiya <ex_ldh0410mrmtsiy@icloud.com>
2021-05-28 03:30:24 +0900

    UI-change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def show
-    
-  end
-  
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -14,13 +14,13 @@ before_action :set_like, only: [:edit, :update, :index, :destory]
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_to posts_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_to posts_path
+    redirect_back(fallback_location: root_path)
   end
 
   private
